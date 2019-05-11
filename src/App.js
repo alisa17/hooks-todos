ab8d3c818eaa3b58c69b0e398242637779193881
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-11 17:06:26 +1200

    Use react context with cotext hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from "react";
-import { UserContext } from "./index";
+import React, {useContext} from "react";
+import {UserContext} from "./index";
 
 export default function App() {
-  return (
-    <div>
-      <UserContext.Consumer>
-        {value => <div>Hello, {value}</div>}
-      </UserContext.Consumer>
-    </div>
-  );
+
+  const value = useContext(UserContext)
+  return <div>Hello, {value}</div>
 }
