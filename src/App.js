76982f8024f5dfbc96e24bf84be9c1e8e61f3532
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-11 17:03:32 +1200

    Set up user context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import { UserContext } from "./index";
 
 export default function App() {
   return (
-    <div>app</div>
-  )
+    <div>
+      <UserContext.Consumer>
+        {value => <div>Hello, {value}</div>}
+      </UserContext.Consumer>
+    </div>
+  );
 }
