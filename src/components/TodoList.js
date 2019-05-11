c4be643cb020b9715eade3f2da8e70c6880e2450
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-12 08:42:02 +1200

    Use both UseContext and UseReducer to make todos

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import TodoContext from "../context";
+
+export default function TodoList() {
+  const { state } = useContext(TodoContext);
+  return (
+    <div>
+      <ul>
+        {state.todos.map(todo => (
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import TodoContext from "./context";
+import todoReducer from "./reducer";
+import TodoList from "./components/TodoList";
 
-const username = "Dave";
-ReactDOM.render(
-    <App />,
-  document.getElementById("root")
-);
+const App = () => {
+  const initialState = useContext(TodoContext);
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  return <TodoContext.Provider value={{ state, dispatch }}>
+    <TodoList/>
+  </TodoContext.Provider>;
+};
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
