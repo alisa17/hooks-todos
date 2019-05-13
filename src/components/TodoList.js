1ee3ee52e55c921f85728162b271d8cb190fb3bb
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-13 22:15:55 +1200

    Add toggle todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import TodoContext from "../context";
 
 export default function TodoList() {
-  const { state } = useContext(TodoContext);
+  const { state, dispatch } = useContext(TodoContext);
   const title =
     state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing to do";
   return (
@@ -10,10 +10,33 @@ export default function TodoList() {
       <h1>{title}</h1>
       <ul className="list-reset text-white p-0">
         {state.todos.map(todo => (
-          <li key={todo.id} className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4">
-            <span className="flex-1 m1-12 cursor-pointer">{todo.text}</span>
-            <button><img src="https://icon.now.sh/edit/0050c5" alt="Edit Icon" className="h-6"/></button>
-              <button><img src="https://icon.now.sh/delete/8b0000" alt="Delete Icon" className="h-6"/></button>
+          <li
+            key={todo.id}
+            className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4"
+          >
+            <span
+              onDoubleClick={() =>
+                dispatch({ type: "TOGGLE_TODO", payload: todo })
+              }
+              className={`flex-1 m1-12 cursor-pointer ${todo.complete &&
+                "line-through text-grey-darkest"}`}
+            >
+              {todo.text}
+            </span>
+            <button>
+              <img
+                src="https://icon.now.sh/edit/0050c5"
+                alt="Edit Icon"
+                className="h-6"
+              />
+            </button>
+            <button>
+              <img
+                src="https://icon.now.sh/delete/8b0000"
+                alt="Delete Icon"
+                className="h-6"
+              />
+            </button>
           </li>
         ))}
       </ul>
