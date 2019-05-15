bfc7791beb5b8afc934eb15e888ca1e1a4e9cae4
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-16 07:58:32 +1200

    Fetch todos from an API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useReducer, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import * as serviceWorker from "./serviceWorker";
 import TodoContext from "./context";
 import todoReducer from "./reducer";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+
+const useAPI = endpoint => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await axios.get(endpoint);
+    setData(response.data);
+  };
+
+  return data;
+};
+
 const App = () => {
   const initialState = useContext(TodoContext);
   const [state, dispatch] = useReducer(todoReducer, initialState);
+  const savedTodos = useAPI("https://hooks-api-f479pj6zp.now.sh/todos");
+
+  useEffect(() => {
+    dispatch({
+      type: "GET_TODOS",
+      payload: savedTodos
+    });
+  }, [savedTodos]);
 
   return (
     <TodoContext.Provider value={{ state, dispatch }}>
