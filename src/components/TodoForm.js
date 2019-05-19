9427f1b6bfe4897f3c0dac79bc55e8d90faeb0a7
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-19 12:48:01 +1200

    Persist updating to server

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -24,7 +24,11 @@ export default function TodoForm() {
   const handleSubmit = async event => {
     event.preventDefault();
     if (currentTodo.text) {
-      dispatch({ type: "UPDATE_TODO", payload: todo });
+      const response = await axios.patch(
+        `https://hooks-api-f479pj6zp.now.sh/todos/${currentTodo.id}`,
+        { text: todo }
+      );
+      dispatch({ type: "UPDATE_TODO", payload: response.data });
     } else {
       const response = await axios.post(
         "https://hooks-api-f479pj6zp.now.sh/todos",
